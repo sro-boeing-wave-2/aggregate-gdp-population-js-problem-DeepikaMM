02890d08eaeaa19b6bbb1a747c81450fa093fb1a
Deepika Mohan <deepikamm12@gmail.com>
2018-07-12 16:58:28 +0530

    logging csv text file

--- aggregate.js
+++ aggregate.js
@@ -14,6 +14,7 @@ const aggregate = (filePath) => {
   }
   const extractMap = new Map();
   const readTXT = fs.readFileSync('data.txt', 'utf8');
+  console.log(readTXT);
   console.log('Listing Files', fs.readdirSync(__dirname));
   const formatReadTXT = readTXT.split('\r\n');
   for (let i = 0; i < formatReadTXT.length - 1; i += 1) {
